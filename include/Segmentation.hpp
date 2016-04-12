c611d017eceb763266046e83b9e3335c1c1420f9
Dimitris Dimou <mijuomij@gmail.com>
2016-04-12 18:31:45 +0300

    Runned clang format

--- include/Segmentation.hpp
+++ include/Segmentation.hpp
@@ -1,58 +1,60 @@
 #ifndef SEGMENTATION_HPP_
 #define SEGMENTATION_HPP_
 
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
 #include "opencv2/core/utility.hpp"
+#include "opencv2/highgui.hpp"
 #include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
 
-#include <stdio.h>
-#include <iostream>
-#include <ctime>
 #include <algorithm>
-#include <vector>
+#include <ctime>
 #include <fstream>
-#include <sstream>
-#include <string>
 #include <initializer_list>
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <vector>
 
 using std::cout;
 
-class ImgSegmenter
-{
+class ImgSegmenter {
 public:
-	ImgSegmenter(void);
-	ImgSegmenter(const cv::Mat &img);
-	ImgSegmenter(cv::Mat &&img);
+  ImgSegmenter(void);
+  ImgSegmenter(const cv::Mat &img);
+  ImgSegmenter(cv::Mat &&img);
 
-    void estimateNormals();// Function to Estimate Normals for Every Point
-    void printNormals();// Function to Print Normals
-    void detectNormalEdges();// Function to Detect Normal Edges and Print cos(thetas)
-    void colorRegions(); // Function to Color the found Regions
-    void writetoFile(std::string filename);
+  void estimateNormals(); // Function to Estimate Normals for Every Point
+  void printNormals();    // Function to Print Normals
+  void
+  detectNormalEdges(); // Function to Detect Normal Edges and Print cos(thetas)
+  void colorRegions(); // Function to Color the found Regions
+  void writetoFile(std::string filename);
 
-	virtual ~ImgSegmenter();
+  virtual ~ImgSegmenter();
 
-	cv::Mat image;          		// Original Input Image
-	cv::Mat median_img;     		// Median Blured Image
-	cv::Mat norm_img;       		// Image with Surface Normals for every pixel
-	cv::Mat norm_color_img; 		// Image with Surface Normals mapped to RGB for every pixel
-	cv::Mat norm_edge_img;  		// Image with painted Edges estimated from Surface Normals
-	cv::Mat norm_bin_edge_img; 	// Image with painted Edges estimated from Surface
-	                       	// Normals
-    cv::Mat colored_img;           	// Image with colored regions
+  cv::Mat image;          // Original Input Image
+  cv::Mat median_img;     // Median Blured Image
+  cv::Mat norm_img;       // Image with Surface Normals for every pixel
+  cv::Mat norm_color_img; // Image with Surface Normals mapped to RGB for every
+                          // pixel
+  cv::Mat
+      norm_edge_img; // Image with painted Edges estimated from Surface Normals
+  cv::Mat norm_bin_edge_img; // Image with painted Edges estimated from Surface
+                             // Normals
+  cv::Mat colored_img;       // Image with colored regions
 
-	int median_kernel;     	// Median Filter Kernel
-	int normal_radius;          	// Radius of Normal Estimation Triangle
-	int normal_step;  	// Kernel to Print Normals
-	int edge_radius; 	// Radius of Surface Normal Edges Detection Window
-	int num_regions;         	// Number of regions found from color scheme
+  int median_kernel; // Median Filter Kernel
+  int normal_radius; // Radius of Normal Estimation Triangle
+  int normal_step;   // Kernel to Print Normals
+  int edge_radius;   // Radius of Surface Normal Edges Detection Window
+  int num_regions;   // Number of regions found from color scheme
 
-	clock_t begin, end;
-	double elapsed_secs;
+  clock_t begin, end;
+  double elapsed_secs;
 
-	std::vector<cv::Point3f> normals; // Vector of Normals for every Point
+  std::vector<cv::Point3f> normals; // Vector of Normals for every Point
 };
 
 #endif // SEGMENTATION_HPP
--- src/Segmentation.cpp
+++ src/Segmentation.cpp
@@ -1,29 +1,29 @@
 #include "segmentation.hpp"
 
-ImgSegmenter::ImgSegmenter () {}
+ImgSegmenter::ImgSegmenter() {}
 
-ImgSegmenter::ImgSegmenter (const cv::Mat &img) : image(img),
-                                            median_kernel(1),
-                                            normal_radius(1),
-                                            normal_step(1),
-                                            edge_radius(1),
-                                            num_regions(0) {}
+ImgSegmenter::ImgSegmenter(const cv::Mat &img)
+    : image(img), median_kernel(1), normal_radius(1), normal_step(1),
+      edge_radius(1), num_regions(0) {}
 
-void ImgSegmenter::estimateNormals (void) {
+void ImgSegmenter::estimateNormals(void) {
   begin = clock();
   cout << "Estimation of Surface Normals begin \n";
 
   normals.reserve((median_img.rows - 2 * normal_radius - 1) *
-                    (median_img.cols - 2 * normal_radius - 1));
+                  (median_img.cols - 2 * normal_radius - 1));
 
   for (int i = normal_radius; i < image.rows - normal_radius; ++i) {
     for (int j = normal_radius; j < image.cols - normal_radius; ++j) {
       // Points a, b, c in the neighborhood of pixel (i, j)
-      cv::Point3f a(i + normal_radius, j - normal_radius,
-                (float)image.at<uchar>(i + normal_radius, j - normal_radius));
-      cv::Point3f b(i + normal_radius, j + normal_radius,
-                (float)image.at<uchar>(i + normal_radius, j + normal_radius));
-      cv::Point3f c(i - normal_radius, j, (float)image.at<uchar>(i - normal_radius, j));
+      cv::Point3f a(
+          i + normal_radius, j - normal_radius,
+          (float)image.at<uchar>(i + normal_radius, j - normal_radius));
+      cv::Point3f b(
+          i + normal_radius, j + normal_radius,
+          (float)image.at<uchar>(i + normal_radius, j + normal_radius));
+      cv::Point3f c(i - normal_radius, j,
+                    (float)image.at<uchar>(i - normal_radius, j));
       cv::Point3f n;
       // Check if pixel is not a valid measurment
       if (a.z == 0 || b.z == 0 || c.z == 0) {
@@ -49,11 +49,11 @@ void ImgSegmenter::estimateNormals (void) {
   }
   end = clock();
   elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-  cout << "Estimation of Surface Normals end \nElapsed time: "
-       << elapsed_secs << "\n";
+  cout << "Estimation of Surface Normals end \nElapsed time: " << elapsed_secs
+       << "\n";
 }
 
-void ImgSegmenter::printNormals (void) {
+void ImgSegmenter::printNormals(void) {
   begin = clock();
   cout << "Printing of Surface Normals begin \n";
 
@@ -89,18 +89,18 @@ void ImgSegmenter::printNormals (void) {
        << "\n";
 }
 
-void ImgSegmenter::detectNormalEdges (void) {
+void ImgSegmenter::detectNormalEdges(void) {
   begin = clock();
   cout << "Detection of Surface Normal Edges begin \n";
 
   norm_edge_img = median_img.clone();
 
-
   norm_bin_edge_img = norm_edge_img.clone();
   for (int i = 0; i < norm_edge_img.rows; ++i) {
     for (int j = 0; j < norm_edge_img.cols; ++j) {
       if (i >= edge_radius && j >= edge_radius &&
-          i < norm_edge_img.rows - edge_radius && j < norm_edge_img.cols - edge_radius) {
+          i < norm_edge_img.rows - edge_radius &&
+          j < norm_edge_img.cols - edge_radius) {
         // Calculate Index
         int index = i * norm_edge_img.cols + j;
         cv::Point3f n0 = normals[index];
@@ -129,16 +129,20 @@ void ImgSegmenter::detectNormalEdges (void) {
           cv::Point3f e = normals[i * norm_edge_img.cols + (j + count)];
           costhetae += n0.dot(e) / edge_radius;
           // North West Direction
-          cv::Point3f nw = normals[(i - count) * norm_edge_img.cols + (j - count)];
+          cv::Point3f nw =
+              normals[(i - count) * norm_edge_img.cols + (j - count)];
           costhetanw += n0.dot(nw) / edge_radius;
           // North East Direction
-          cv::Point3f ne = normals[(i - count) * norm_edge_img.cols + (j + count)];
+          cv::Point3f ne =
+              normals[(i - count) * norm_edge_img.cols + (j + count)];
           costhetane += n0.dot(ne) / edge_radius;
           // South West Direction
-          cv::Point3f sw = normals[(i + count) * norm_edge_img.cols + (j - count)];
+          cv::Point3f sw =
+              normals[(i + count) * norm_edge_img.cols + (j - count)];
           costhetasw += n0.dot(sw) / edge_radius;
           // South East Direction
-          cv::Point3f se = normals[(i + count) * norm_edge_img.cols + (j + count)];
+          cv::Point3f se =
+              normals[(i + count) * norm_edge_img.cols + (j + count)];
           costhetase += n0.dot(se) / edge_radius;
         }
         std::vector<float> thetas{costhetan,  costhetas,  costhetaw,
@@ -158,8 +162,6 @@ void ImgSegmenter::detectNormalEdges (void) {
     }
   }
 
-
-
   cv::medianBlur(norm_bin_edge_img, norm_bin_edge_img, 5);
 
   end = clock();
@@ -168,14 +170,15 @@ void ImgSegmenter::detectNormalEdges (void) {
        << elapsed_secs << "\n";
 }
 
-void ImgSegmenter::colorRegions (void) {
+void ImgSegmenter::colorRegions(void) {
   begin = clock();
   cout << "Region Growing begin \n";
 
-  std::vector<cv::Scalar> colors{cv::Scalar(0, 0, 255),   cv::Scalar(0, 255, 0),
-                             cv::Scalar(255, 0, 0),   cv::Scalar(255, 0, 255),
-                             cv::Scalar(0, 255, 255), cv::Scalar(255, 255, 0)};
-  
+  std::vector<cv::Scalar> colors{
+      cv::Scalar(0, 0, 255),   cv::Scalar(0, 255, 0),
+      cv::Scalar(255, 0, 0),   cv::Scalar(255, 0, 255),
+      cv::Scalar(0, 255, 255), cv::Scalar(255, 255, 0)};
+
   for (int i = 0; i < colored_img.rows; ++i) {
     for (int j = 0; j < colored_img.cols; ++j) {
       cv::Point seed;
@@ -186,20 +189,24 @@ void ImgSegmenter::colorRegions (void) {
       }
     }
   }
-  
+
   int ke = 7;
   for (int r = 0; r < colored_img.rows; ++r) {
     for (int c = 0; c < colored_img.cols; ++c) {
       if (colored_img.at<cv::Vec3b>(r, c) == cv::Vec3b(0, 0, 0)) {
         cv::Point min_distp = cv::Point(0, 0);
         float min_dist = 3;
-        for (int i = -ke/2; i <= ke/2; ++i) {
-          for (int j = -ke/2; j < ke/2; ++j) {
-            int image_r = std::min(std::max(r + i, 0), (int)(colored_img.rows - 1));
-            int image_c = std::min(std::max(c + j, 0), (int)(colored_img.cols - 1));
-            if (colored_img.at<cv::Vec3b>(image_r, image_c) != cv::Vec3b(0, 0, 0)) {
-              float depth_dif = abs((int)median_img.at<uchar>(image_r, image_c) -
-                                (int)median_img.at<uchar>(r, c));
+        for (int i = -ke / 2; i <= ke / 2; ++i) {
+          for (int j = -ke / 2; j < ke / 2; ++j) {
+            int image_r =
+                std::min(std::max(r + i, 0), (int)(colored_img.rows - 1));
+            int image_c =
+                std::min(std::max(c + j, 0), (int)(colored_img.cols - 1));
+            if (colored_img.at<cv::Vec3b>(image_r, image_c) !=
+                cv::Vec3b(0, 0, 0)) {
+              float depth_dif =
+                  abs((int)median_img.at<uchar>(image_r, image_c) -
+                      (int)median_img.at<uchar>(r, c));
               float euc_dist = sqrt(pow(i, 2) + pow(j, 2) + pow(depth_dif, 2));
               if (euc_dist < min_dist) {
                 min_dist = euc_dist;
@@ -209,9 +216,10 @@ void ImgSegmenter::colorRegions (void) {
           }
         }
         if (min_distp != cv::Point(0, 0)) {
-          colored_img.at<cv::Vec3b>(r, c) = colored_img.at<cv::Vec3b>(min_distp);
+          colored_img.at<cv::Vec3b>(r, c) =
+              colored_img.at<cv::Vec3b>(min_distp);
         }
-      } 
+      }
     }
   }
 
@@ -222,59 +230,64 @@ void ImgSegmenter::colorRegions (void) {
   cout << "Elapsed time: " << elapsed_secs << "\n";
 }
 
-void ImgSegmenter::writetoFile (std::string filename) { 
+void ImgSegmenter::writetoFile(std::string filename) {
   std::ofstream myfile;
-  myfile.open (filename + ".obj");
-  
+  myfile.open(filename + ".obj");
+
   int count_i(0);
   int count_j(0);
 
   for (int i = 0; i < colored_img.rows; ++i) {
     for (int j = 0; j < colored_img.cols; ++j) {
       if ((i % 2 == 0) && (j % 2 == 0)) {
-        //cout << "I = " << i << "J = " << j << "\n";
-        myfile << "v " << ((colored_img.cols / 2.0) - j) / colored_img.cols << " "
-        << ((colored_img.rows / 2.0) - i) / colored_img.rows << " "
-        << (int)median_img.at<uchar>(i, j) / 255. << "\n";
+        // cout << "I = " << i << "J = " << j << "\n";
+        myfile << "v " << ((colored_img.cols / 2.0) - j) / colored_img.cols
+               << " " << ((colored_img.rows / 2.0) - i) / colored_img.rows
+               << " " << (int)median_img.at<uchar>(i, j) / 255. << "\n";
         myfile << "c " << colored_img.at<cv::Vec3b>(i, j)[0] / 255 << " "
-        << colored_img.at<cv::Vec3b>(i, j)[1] / 255 << " "
-        << colored_img.at<cv::Vec3b>(i, j)[2] / 255 << "\n";
-        if ( (i <= colored_img.rows - 2) && (j <= colored_img.cols - 4)) {
-          myfile << "f " << (count_i*(colored_img.cols/2)+j/2) << " "
-                         << (count_i*(colored_img.cols/2)+(j/2+1)) << " "
-                         << ((count_i+1)*(colored_img.cols/2)+(j/2+1))
-                         << "\n";
-          myfile << "f " << (count_i*(colored_img.cols/2)+j/2) << " "
-                         << ((count_i+1)*(colored_img.cols/2)+j/2) << " "
-                         << ((count_i+1)*(colored_img.cols/2)+(j/2+1))
-                         << "\n";
+               << colored_img.at<cv::Vec3b>(i, j)[1] / 255 << " "
+               << colored_img.at<cv::Vec3b>(i, j)[2] / 255 << "\n";
+        if ((i <= colored_img.rows - 2) && (j <= colored_img.cols - 4)) {
+          myfile << "f " << (count_i * (colored_img.cols / 2) + j / 2) << " "
+                 << (count_i * (colored_img.cols / 2) + (j / 2 + 1)) << " "
+                 << ((count_i + 1) * (colored_img.cols / 2) + (j / 2 + 1))
+                 << "\n";
+          myfile << "f " << (count_i * (colored_img.cols / 2) + j / 2) << " "
+                 << ((count_i + 1) * (colored_img.cols / 2) + j / 2) << " "
+                 << ((count_i + 1) * (colored_img.cols / 2) + (j / 2 + 1))
+                 << "\n";
         }
-      } 
-      //if (j % 2 == 0) ++count_j;
+      }
+      // if (j % 2 == 0) ++count_j;
     }
-    if (i % 2 == 0) ++count_i;
+    if (i % 2 == 0)
+      ++count_i;
   }
   cout << "count_i = " << count_i << " count_j = " << count_j << "\n";
-  cout << colored_img.rows << " " << (int)colored_img.cols/2 << "\n";
+  cout << colored_img.rows << " " << (int)colored_img.cols / 2 << "\n";
   myfile.close();
 }
 
-ImgSegmenter::~ImgSegmenter () {}
+ImgSegmenter::~ImgSegmenter() {}
 
 int prepare_segmentation(cv::Mat &img) {
 
   ImgSegmenter segmenter(img);
 
   // Median Filter for Noise Reduction
-  cv::medianBlur(segmenter.image, segmenter.median_img, segmenter.median_kernel);
+  cv::medianBlur(segmenter.image, segmenter.median_img,
+                 segmenter.median_kernel);
 
   // Estimation of Surface Normals
   segmenter.estimateNormals();
 
   // Prints Normal Vector at every pixel and Map xyz to RGB
 
-  cv::Rect crop(segmenter.normal_radius, segmenter.normal_radius, segmenter.norm_color_img.cols - 2 * segmenter.normal_radius,
-            segmenter.norm_color_img.rows - 2 * segmenter.normal_radius); // Crop image according to normal_radius
+  cv::Rect crop(
+      segmenter.normal_radius, segmenter.normal_radius,
+      segmenter.norm_color_img.cols - 2 * segmenter.normal_radius,
+      segmenter.norm_color_img.rows -
+          2 * segmenter.normal_radius); // Crop image according to normal_radius
 
   segmenter.norm_color_img = segmenter.norm_color_img(crop);
   segmenter.norm_img = segmenter.norm_img(crop);
@@ -286,13 +299,13 @@ int prepare_segmentation(cv::Mat &img) {
   segmenter.norm_edge_img = segmenter.norm_edge_img(crop);
   segmenter.detectNormalEdges();
   crop = cv::Rect(segmenter.edge_radius, segmenter.edge_radius,
-              segmenter.norm_edge_img.cols - 2 * segmenter.edge_radius,
-              segmenter.norm_edge_img.rows - 2 * segmenter.edge_radius);
+                  segmenter.norm_edge_img.cols - 2 * segmenter.edge_radius,
+                  segmenter.norm_edge_img.rows - 2 * segmenter.edge_radius);
   segmenter.norm_edge_img = segmenter.norm_edge_img(crop);
   segmenter.norm_bin_edge_img = segmenter.norm_bin_edge_img(crop);
 
   cvtColor(segmenter.norm_bin_edge_img, segmenter.colored_img, CV_GRAY2RGB);
-  
+
   segmenter.colorRegions();
 
   return 0;
