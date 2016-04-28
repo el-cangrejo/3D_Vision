5e075875bed6b5d8f2d4ee3b2368c13f979193e0
Dimitris Dimou <mijuomij@gmail.com>
2016-04-28 17:30:33 +0300

    Added action TakeSnapshot

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -37,6 +37,7 @@ private slots:
   void onActionOpenKinect();
   void onActionLoadDatabase();
   void onActionPreprocessDatabase();
+  void onActionTakeSnapshot();
 
   /*
    * Color Setting Functions
