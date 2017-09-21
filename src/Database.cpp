b30327686bff2b860acaf722875b2132f4b2493c
dimitrisd <mijuomij@gmail.com>
2017-09-21 13:01:29 +0300

    Fixed logical error

--- src/Database.cpp
+++ src/Database.cpp
@@ -1,25 +1,22 @@
 #include "Database.hpp"
 
-
-void loadDB(const std::string db_path, std::vector<Mesh> &meshes) {
+void loadDB(std::string db_path, std::vector<Mesh> &meshes) {
   boost::filesystem::path p(db_path);
 
   for (boost::filesystem::directory_iterator dir(p);
        dir != boost::filesystem::directory_iterator(); dir++) {
-    if (!boost::filesystem::is_regular_file(*dir))
-      continue;
 
     std::string db_entry = (*dir).path().string();
-
-    if (db_entry.substr(db_entry.size() - 3, 3) != "obj" || 
+    if (db_entry.substr(db_entry.size() - 3, 3) != "obj" && 
 				db_entry.substr(db_entry.size() - 3, 3) != "off")
       continue;
 
     Mesh m;
 		m.load(db_entry);
 		m.preprocess();
+		if (!m.triangles.empty()) m.computeNormals();
 
 		meshes.push_back(m);
   }
-  std::cout << "Meshes load to database for display : " << meshes.size() << "\n";
+	std::cout << "\e[1mMeshes load to database for display : " << meshes.size() << "\e[0m\n";
 }
